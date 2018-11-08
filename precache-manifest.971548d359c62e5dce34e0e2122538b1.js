0f19df5967456350726f56c7c6666e6249377519
jerrylzy <jerrylzy@ucla.edu>
2018-11-08 08:48:13 -0800

    Updates

---
+++ precache-manifest.971548d359c62e5dce34e0e2122538b1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "51dff7aa7f9244a53d8a",
+    "url": "/BruinMart/static/css/main.39ef90fd.chunk.css"
+  },
+  {
+    "revision": "51dff7aa7f9244a53d8a",
+    "url": "/BruinMart/static/js/main.51dff7aa.chunk.js"
+  },
+  {
+    "revision": "a8553ea6c0534580db2b",
+    "url": "/BruinMart/static/css/1.fd071f18.chunk.css"
+  },
+  {
+    "revision": "a8553ea6c0534580db2b",
+    "url": "/BruinMart/static/js/1.a8553ea6.chunk.js"
+  },
+  {
+    "revision": "ac8e0db6df00fcca4efa",
+    "url": "/BruinMart/static/js/runtime~main.ac8e0db6.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/BruinMart/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/BruinMart/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/BruinMart/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/BruinMart/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/BruinMart/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "90d7d694c972abee30cec9e457c1f72f",
+    "url": "/BruinMart/index.html"
+  }
+];
