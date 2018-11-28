cd5f64d7fff8c2427473e51a970fb338ec25c5a9
Ashley <Ashley@appledeMacBook-Pro-3.local>
2018-11-27 20:46:43 -0800

    move login modal to login component

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,115 +1,123 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-    Navbar,
-    NavbarBrand,
-    Nav,
-    NavbarToggler,
-    Collapse,
-    NavItem,
-    Jumbotron,
-    Button,
-    ModalHeader,
-    Modal,
-    ModalBody,
-    Form,
-    FormGroup,
-    Input,
-    Label
-} from 'reactstrap';
-import { NavLink } from 'react-router-dom';
-import Avatar from 'react-avatar';
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavbarToggler,
+  Collapse,
+  NavItem,
+  Jumbotron,
+  Button,
+  ModalHeader,
+  Modal,
+  ModalBody,
+  Form,
+  FormGroup,
+  Input,
+  Label
+} from "reactstrap";
+
+import Login from "./LoginComponent";
+
+import { NavLink } from "react-router-dom";
+import Avatar from "react-avatar";
 
 class Header extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            isNavOpen: false,
-            isModalOpen: false
-        };
+    this.state = {
+      isNavOpen: false,
+      isModalOpen: false
+    };
 
-        this.toggleNav = this.toggleNav.bind(this);
-        this.toggleModal = this.toggleModal.bind(this);
-        this.handleLogin = this.handleLogin.bind(this);
+    this.toggleNav = this.toggleNav.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
 
-    }
+  handleLogin(event) {
+    this.toggleModal();
+    alert(
+      "Username: " +
+        this.username.value +
+        " Password: " +
+        this.password.value +
+        " Remember: " +
+        this.remember.checked
+    );
+    event.preventDefault();
+  }
 
-    handleLogin(event) {
-        this.toggleModal();
-        alert('Username: ' + this.username.value + ' Password: ' + this.password.value + ' Remember: ' + this.remember.checked);
-        event.preventDefault();
-    }
+  toggleNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen
+    });
+  }
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  }
 
-    toggleNav() {
-        this.setState({
-            isNavOpen: !this.state.isNavOpen
-        });
-    }
-    toggleModal() {
-        this.setState({
-            isModalOpen: !this.state.isModalOpen
-        });
-    }
-
-    render() {
-        return (
-            <>
-                <Navbar dark fixed="top">
-                    <div className="container">
-                        <NavbarToggler onClick={this.toggleNav} />
-                        <NavbarBrand className="mr-auto">
-                            <Avatar size={50} src="assets/images/joe_bruin.jpg" round={true} />
-                        </NavbarBrand>
-                        <Collapse isOpen={this.state.isNavOpen} navbar>
-                            <Nav navbar>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/profile">
-                                        <h4>Joe Bruin</h4>
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/profile">
-                                        <span className="fa fa-user fa-lg" /> My Profile
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/posts">
-                                        <span className="fa fa-stack-exchange fa-lg" /> My Posts
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/saved">
-                                        <span className="fa fa-heart-o fa-lg" /> Saved Items
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/ongoing">
-                                        <span className="fa fa-exchange fa-lg" /> Ongoing
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/sold">
-                                        <span className="fa fa-shopping-bag fa-lg" /> Sold
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/settings">
-                                        <span className="fa fa-cog fa-lg" /> Settings
-                                    </NavLink>
-                                </NavItem>
-                            </Nav>
-                        </Collapse>
-                        <Nav className="ml-auto" navbar>
-                                <NavItem>
-                                    <Button outline onClick={this.toggleModal}>
-                                        <span className="fa fa-sign-in fa-lg" /> Login
-                                    </Button>
-                                </NavItem>
-                            </Nav>
-
-
-                        
-                        {/* <Nav className="md-mx-auto">
+  render() {
+    return (
+      <>
+        <Navbar dark fixed="top">
+          <div className="container">
+            <NavbarToggler onClick={this.toggleNav} />
+            <NavbarBrand className="mr-auto">
+              <Avatar
+                size={50}
+                src="assets/images/joe_bruin.jpg"
+                round={true}
+              />
+            </NavbarBrand>
+            <Collapse isOpen={this.state.isNavOpen} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink className="nav-link" to="/profile">
+                    <h4>Joe Bruin</h4>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/profile">
+                    <span className="fa fa-user fa-lg" /> My Profile
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/posts">
+                    <span className="fa fa-stack-exchange fa-lg" /> My Posts
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/saved">
+                    <span className="fa fa-heart-o fa-lg" /> Saved Items
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/ongoing">
+                    <span className="fa fa-exchange fa-lg" /> Ongoing
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/sold">
+                    <span className="fa fa-shopping-bag fa-lg" /> Sold
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/settings">
+                    <span className="fa fa-cog fa-lg" /> Settings
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <Login />
+              </NavItem>
+            </Nav>
+            {/* <Nav className="md-mx-auto">
                                 <NavItem>
                                     <NavLink className="nav-link nav-buttons" to="/home">
                                         <span className="fa fa-shopping-bag fa-lg" />
@@ -126,44 +134,24 @@ class Header extends Component {
                                     </NavLink>
                                 </NavItem>
                             </Nav> */}
-                    </div>
-                </Navbar>
-                <Jumbotron>
-                    <div className="container">
-                        <div className="row row-header">
-                            <div className="col-12 col-sm-6">
-                                <h1>BruinMart</h1>
-                                <p>We want to redesign the user experience for using the Facebook "Free and For Sale" group for both buyers and sellers. </p>
-                            </div>
-                        </div>
-                    </div>
-                </Jumbotron>
-                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
-                    <ModalBody>
-                        <Form onSubmit={this.handleLogin}>
-                            <FormGroup>
-                                <Label htmlFor="username">Username</Label>
-                                <Input type="text" id="username" name="username" innerRef={input => (this.username = input)} />
-                            </FormGroup>
-                            <FormGroup>
-                                <Label htmlFor="password">Password</Label>
-                                <Input type="password" id="password" name="password" innerRef={input => (this.password = input)} />
-                            </FormGroup>
-                            <FormGroup check>
-                                <Label check>
-                                    <Input type="checkbox" name="remember" innerRef={input => (this.remember = input)} /> Remember me
-                                </Label>
-                            </FormGroup>
-                            <Button type="submit" value="submit" color="primary">
-                                Login
-                            </Button>
-                        </Form>
-                    </ModalBody>
-                </Modal>
-            </>
-        );
-    }
+          </div>
+        </Navbar>
+        <Jumbotron>
+          <div className="container">
+            <div className="row row-header">
+              <div className="col-12 col-sm-6">
+                <h1>BruinMart</h1>
+                <p>
+                  We want to redesign the user experience for using the Facebook
+                  "Free and For Sale" group for both buyers and sellers.{" "}
+                </p>
+              </div>
+            </div>
+          </div>
+        </Jumbotron>
+      </>
+    );
+  }
 }
 
 export default Header;
--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,102 +1,84 @@
-import React, { Component } from 'react';
-import { Breadcrumb, BreadcrumbItem, Form, FormGroup, Label, Input, Col, Button } from 'reactstrap';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import React, { Component } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import {
+  Button,
+  ModalHeader,
+  Modal,
+  ModalBody,
+  Form,
+  FormGroup,
+  Input,
+  Label
+} from "reactstrap";
 
-class Login extends Component {
-    constructor(props) {
-        super(props);
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            username: '',
-            password: ''
-        };
-        this.handleRegister = this.handleRegister.bind(this);
-        this.handleOnSubmit = this.props.onSubmit.bind(this);
-        this.handleInputChange = this.handleInputChange.bind(this);
-    }
+    this.state = {
+      modal: false
+    };
 
-    handleRegister(event) {
-        event.preventDefault();
+    this.toggle = this.toggle.bind(this);
+  }
 
-        //  TODO: Use redirect
-        window.location.assign('/register');
-    }
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
 
-    handleInputChange(event) {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
-
-        this.setState({
-            [name]: value
-        });
-    }
-
-    render() {
-        return (
-            <div className="container login-div">
-                <div className="container logo-div">
-                    <div className="row">
-                        <div className="col-1" />
-                        <div className="col-10">
-                            <div className="row">
-                                <img height="100" width="100" src="assets/images/marketplace.png" />
-                                <h1 className="justify-content-center align-self-center">BruinMart</h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="container">
-                    <div className="col-12 col-md-9">
-                        <Form onSubmit={this.handleOnSubmit}>
-                            <FormGroup row>
-                                <Label htmlFor="username" xs={2}>
-                                    <span className="fa fa-user fa-lg" />
-                                </Label>
-                                <Col xs={10}>
-                                    <Input
-                                        type="text"
-                                        id="username"
-                                        name="username"
-                                        placeholder="username"
-                                        value={this.state.username}
-                                        onChange={this.handleInputChange}
-                                    />
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label htmlFor="password" xs={2}>
-                                    <span className="fa fa-lock fa-lg" />
-                                </Label>
-                                <Col xs={10}>
-                                    <Input
-                                        type="password"
-                                        id="password"
-                                        name="password"
-                                        placeholder="password"
-                                        value={this.state.password}
-                                        onChange={this.handleInputChange}
-                                    />
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Col xs={{ size: 4, offset: 2 }}>
-                                    <Button block type="button" color="secondary" onClick={this.handleRegister}>
-                                        Register
-                                    </Button>
-                                </Col>
-                                <Col xs={{ size: 4, offset: 2 }}>
-                                    <Button block type="submit" color="primary">
-                                        Login
-                                    </Button>
-                                </Col>
-                            </FormGroup>
-                        </Form>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Button outline onClick={this.toggle}>
+          <span className="fa fa-sign-in fa-lg" /> Login
+        </Button>
+        <Modal
+          isOpen={this.state.modal}
+          toggle={this.toggle}
+          className={this.props.className}
+        >
+          <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  type="text"
+                  id="username"
+                  name="username"
+                  innerRef={input => (this.username = input)}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  type="password"
+                  id="password"
+                  name="password"
+                  innerRef={input => (this.password = input)}
+                />
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input
+                    type="checkbox"
+                    name="remember"
+                    innerRef={input => (this.remember = input)}
+                  />{" "}
+                  Remember me
+                </Label>
+              </FormGroup>
+              <Button type="submit" value="submit" color="primary">
+                Login
+              </Button>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default Login;
