fde0e4f995393c76fa92d36fa6d827b52ece85c5
jerrylzy <jerrylzy@ucla.edu>
2018-11-29 01:41:03 -0800

    Enlarge the dropzone

--- src/components/NewPostComponent.jsx
+++ src/components/NewPostComponent.jsx
@@ -1,51 +1,78 @@
 import React from 'react';
-import { Button,Form, FormGroup, Label, Input, FormText ,Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import {
+    Button,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    FormText,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter
+} from 'reactstrap';
 import ImageUpload from './ImageUploadComponent';
 
 const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' }
-]
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+];
 
 class ModalExample extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal: false,
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: false
+        };
 
-    this.toggle = this.toggle.bind(this);
-  }
+        this.toggle = this.toggle.bind(this);
+    }
 
-  toggle() {
-    this.setState({
-      modal: !this.state.modal
-    });
-  }
+    toggle() {
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
 
-  render() {
-    return (
-      <div>
-        <Button style={{ background: "transparent" }} onClick={this.toggle}><span className="fa fa-plus fa-lg" /></Button>
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className="modal-lg">
-          <ModalHeader toggle={this.toggle}>New Post</ModalHeader>
-          <ModalBody>
-          <Form>
-        <FormGroup>
-          <Label for="Item">Item</Label>
-          <Input type="text" name="item" id="itemName" placeholder="enter the name of the item you want to sell" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="Price">Price</Label>
-          <Input type="text" name="price" id="itemPrice" placeholder="enter a number in US dollars" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="itemDes">Item Description</Label>
-          <Input type="textarea" name="itemDes" id="itemDes" 
-          placeholder="enter a few lines to describe your item and outline anything potential buyers may need to know" />
-        </FormGroup>
-        {/* <FormGroup>
+    render() {
+        return (
+            <div>
+                <Button style={{ background: 'transparent' }} onClick={this.toggle}>
+                    <span className="fa fa-plus fa-lg" />
+                </Button>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className="modal-lg">
+                    <ModalHeader toggle={this.toggle}>New Post</ModalHeader>
+                    <ModalBody>
+                        <Form>
+                            <FormGroup>
+                                <Label for="Item">Item</Label>
+                                <Input
+                                    type="text"
+                                    name="item"
+                                    id="itemName"
+                                    placeholder="enter the name of the item you want to sell"
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="Price">Price</Label>
+                                <Input
+                                    type="text"
+                                    name="price"
+                                    id="itemPrice"
+                                    placeholder="enter a number in US dollars"
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="itemDes">Item Description</Label>
+                                <Input
+                                    type="textarea"
+                                    name="itemDes"
+                                    id="itemDes"
+                                    placeholder="enter a few lines to describe your item and outline anything potential buyers may need to know"
+                                />
+                            </FormGroup>
+                            {/* <FormGroup>
           <Label for="exampleSelectMulti">Select Multiple</Label>
           <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
             <option>1</option>
@@ -55,15 +82,13 @@ class ModalExample extends React.Component {
             <option>5</option>
           </Input>
         </FormGroup> */}
-        <FormGroup>
-          <Label for="exampleFile">File</Label>
-          {/* <Input type="file" name="file" id="exampleFile" /> */}
-          <ImageUpload />
-          <FormText color="muted">
-            Upload a picture for your item here.
-          </FormText>
-        </FormGroup>
-        {/* <FormGroup tag="fieldset">
+                            <FormGroup>
+                                <Label for="exampleFile">File</Label>
+                                {/* <Input type="file" name="file" id="exampleFile" /> */}
+                                <ImageUpload />
+                                <FormText color="muted">Upload a picture for your item here.</FormText>
+                            </FormGroup>
+                            {/* <FormGroup tag="fieldset">
           <legend>Radio Buttons</legend>
           <FormGroup check>
             <Label check>
@@ -84,22 +109,26 @@ class ModalExample extends React.Component {
             </Label>
           </FormGroup>
         </FormGroup> */}
-        <FormGroup check>
-          <Label check>
-            <Input type="checkbox" />{' '}
-            I understand that my information will only be provided to any potential buyers after they have reserved my item.
-          </Label>
-        </FormGroup>
-      </Form>
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Post</Button>{' '}
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
-      </div>
-    );
-  }
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" /> I understand that my information will only be provided to
+                                    any potential buyers after they have reserved my item.
+                                </Label>
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.toggle}>
+                            Post
+                        </Button>{' '}
+                        <Button color="secondary" onClick={this.toggle}>
+                            Cancel
+                        </Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+        );
+    }
 }
 
 export default ModalExample;
