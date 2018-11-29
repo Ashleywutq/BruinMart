079433dc6061fc78de226d346dfd68038f9a8b8a
Jiayu Zhao <jyzhao1230@g.ucla.edu>
2018-11-28 22:49:11 -0800

    Updated SideBar to Close When Clicked on Each NavItem

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,102 +1,103 @@
-import React, { Component } from 'react';
-import Home from './HomeComponent';
-import Profile from './ProfileComponent';
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
-import SideBar from './SideBarComponent';
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { filterResults, fetchItems } from '../redux/ActionCreators';
+import React, { Component } from "react";
+import Home from "./HomeComponent";
+import Profile from "./ProfileComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import SideBar from "./SideBarComponent";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { filterResults, fetchItems } from "../redux/ActionCreators";
 
-import '../style.css';
+import "../style.css";
 
-const mapStateToProps = (state) => {
-    return {
-        sellItems: state.sellItems,
-        users: state.users
-    };
+const mapStateToProps = state => {
+  return {
+    sellItems: state.sellItems,
+    users: state.users
+  };
 };
 
-const mapDispatchToProps = (dispatch) => ({
-    filterResults: (searchText) => {
-        dispatch(filterResults(searchText));
-    },
-    fetchItems: () => {
-        dispatch(fetchItems());
-    }
+const mapDispatchToProps = dispatch => ({
+  filterResults: searchText => {
+    dispatch(filterResults(searchText));
+  },
+  fetchItems: () => {
+    dispatch(fetchItems());
+  }
 });
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            loggedIn: false,
-            isSideNavOpen: false
-        };
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.toggleSideNav = this.toggleSideNav.bind(this);
-        this.handleStateChange = this.handleStateChange.bind(this);
-    }
+    this.state = {
+      loggedIn: false,
+      isSideNavOpen: false
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.toggleSideNav = this.toggleSideNav.bind(this);
+    this.handleStateChange = this.handleStateChange.bind(this);
+  }
 
-    componentDidMount() {
-        this.props.fetchItems();
-    }
+  componentDidMount() {
+    this.props.fetchItems();
+  }
 
-    handleStateChange(state) {
-        this.setState({
-            isSideNavOpen: state.isOpen
-        });
-    }
+  handleStateChange(state) {
+    this.setState({
+      isSideNavOpen: state.isOpen
+    });
+  }
 
-    toggleSideNav() {
-        this.setState({
-            isSideNavOpen: !this.state.isSideNavOpen
-        });
-    }
+  toggleSideNav() {
+    this.setState({
+      isSideNavOpen: !this.state.isSideNavOpen
+    });
+  }
 
-    handleSubmit(event) {
-        this.setState({
-            loggedIn: true
-        });
-        event.preventDefault();
-    }
+  handleSubmit(event) {
+    this.setState({
+      loggedIn: true
+    });
+    event.preventDefault();
+  }
 
-    render() {
-        const HomePage = () => {
-            return <Home sellItems={this.props.sellItems.sellItems} />;
-        };
+  render() {
+    const HomePage = () => {
+      return <Home sellItems={this.props.sellItems.sellItems} />;
+    };
 
-        return (
-            <div id="App">
-                <SideBar
-                    noOverlay
-                    pageWrapId={'page-wrapper'}
-                    outerContainerId={'App'}
-                    isOpen={this.state.isSideNavOpen}
-                    onStateChange={(state) => this.handleStateChange(state)}
-                />
-                <div id="page-wrapper">
-                    <Header
-                        users={this.props.users}
-                        filterResults={this.props.filterResults}
-                        fetchItems={this.props.fetchItems}
-                        toggleSideNav={this.toggleSideNav}
-                    />
-                    <Switch>
-                        <Route exact path="/" component={HomePage} />
-                        <Route path="/profile" component={Profile} />
-                        <Redirect to="/" />
-                    </Switch>
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div id="App">
+        <SideBar
+          noOverlay
+          pageWrapId={"page-wrapper"}
+          outerContainerId={"App"}
+          isOpen={this.state.isSideNavOpen}
+          onStateChange={state => this.handleStateChange(state)}
+          toggleSideNav={this.toggleSideNav}
+        />
+        <div id="page-wrapper">
+          <Header
+            users={this.props.users}
+            filterResults={this.props.filterResults}
+            fetchItems={this.props.fetchItems}
+            toggleSideNav={this.toggleSideNav}
+          />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/profile" component={Profile} />
+            <Redirect to="/" />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default withRouter(
-    connect(
-        mapStateToProps,
-        mapDispatchToProps
-    )(Main)
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Main)
 );
--- src/components/SideBarComponent.jsx
+++ src/components/SideBarComponent.jsx
@@ -1,71 +1,71 @@
-import React from 'react';
+import React from "react";
 import {
-    Navbar,
-    NavbarBrand,
-    Nav,
-    NavbarToggler,
-    Collapse,
-    NavItem,
-    Jumbotron,
-    Button,
-    ModalHeader,
-    Modal,
-    ModalBody,
-    Form,
-    FormGroup,
-    Input,
-    Label
-} from 'reactstrap';
-import { slide as Menu } from 'react-burger-menu';
-import { NavLink } from 'react-router-dom';
-import Avatar from 'react-avatar';
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavbarToggler,
+  Collapse,
+  NavItem,
+  Jumbotron,
+  Button,
+  ModalHeader,
+  Modal,
+  ModalBody,
+  Form,
+  FormGroup,
+  Input,
+  Label
+} from "reactstrap";
+import { slide as Menu } from "react-burger-menu";
+import { NavLink } from "react-router-dom";
+import Avatar from "react-avatar";
 
-const SideBar = (props) => {
-    return (
-        // Pass on our props
-        <Menu {...props}>
-            <Nav navbar>
-                <NavbarBrand className="mr-auto col-2 col-sm-2">
-                    <Avatar size={50} src="assets/images/joe_bruin.jpg" round={true} />
-                </NavbarBrand>
-                <NavItem>
-                    <NavLink className="nav-link" to="/profile">
-                        <h4>Joe Bruin</h4>
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink className="nav-link" to="/profile">
-                        <span className="fa fa-user fa-lg" /> My Profile
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink className="nav-link" to="/posts">
-                        <span className="fa fa-stack-exchange fa-lg" /> My Posts
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink className="nav-link" to="/saved">
-                        <span className="fa fa-heart-o fa-lg" /> Saved Items
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink className="nav-link" to="/ongoing">
-                        <span className="fa fa-exchange fa-lg" /> Ongoing
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink className="nav-link" to="/sold">
-                        <span className="fa fa-shopping-bag fa-lg" /> Sold
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink className="nav-link" to="/settings">
-                        <span className="fa fa-cog fa-lg" /> Settings
-                    </NavLink>
-                </NavItem>
-            </Nav>
-        </Menu>
-    );
+const SideBar = props => {
+  return (
+    // Pass on our props
+    <Menu {...props}>
+      <Nav navbar onClick={props.toggleSideNav}>
+        <NavbarBrand className="mr-auto col-2 col-sm-2">
+          <Avatar size={50} src="assets/images/joe_bruin.jpg" round={true} />
+        </NavbarBrand>
+        <NavItem>
+          <NavLink className="nav-link" to="/profile">
+            <h4>Joe Bruin</h4>
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className="nav-link" to="/profile">
+            <span className="fa fa-user fa-lg" /> My Profile
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className="nav-link" to="/posts">
+            <span className="fa fa-stack-exchange fa-lg" /> My Posts
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className="nav-link" to="/saved">
+            <span className="fa fa-heart-o fa-lg" /> Saved Items
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className="nav-link" to="/ongoing">
+            <span className="fa fa-exchange fa-lg" /> Ongoing
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className="nav-link" to="/sold">
+            <span className="fa fa-shopping-bag fa-lg" /> Sold
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className="nav-link" to="/settings">
+            <span className="fa fa-cog fa-lg" /> Settings
+          </NavLink>
+        </NavItem>
+      </Nav>
+    </Menu>
+  );
 };
 
 export default SideBar;
