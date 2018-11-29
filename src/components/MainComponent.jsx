8e0f36a1c992966fa735a4226d23b6550b1278b3
Ashley <ashley-wu@live.com>
2018-11-28 22:17:45 -0800

    add profile component

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,74 +1,76 @@
-import React, { Component } from 'react';
-import Home from './HomeComponent';
-import Login from './LoginComponent';
-import Register from './RegisterComponent';
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { filterResults, fetchItems } from '../redux/ActionCreators';
+import React, { Component } from "react";
+import Home from "./HomeComponent";
+import Profile from "./ProfileComponent";
+import Login from "./LoginComponent";
+import Register from "./RegisterComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { filterResults, fetchItems } from "../redux/ActionCreators";
 
-const mapStateToProps = (state) => {
-    return {
-        sellItems: state.sellItems,
-        users: state.users
-    };
+const mapStateToProps = state => {
+  return {
+    sellItems: state.sellItems,
+    users: state.users
+  };
 };
 
-const mapDispatchToProps = (dispatch) => ({
-    filterResults: (searchText) => {
-        dispatch(filterResults(searchText));
-    },
-    fetchItems: () => {
-        dispatch(fetchItems());
-    }
+const mapDispatchToProps = dispatch => ({
+  filterResults: searchText => {
+    dispatch(filterResults(searchText));
+  },
+  fetchItems: () => {
+    dispatch(fetchItems());
+  }
 });
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            loggedIn: false
-        };
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    this.state = {
+      loggedIn: false
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    componentDidMount() {
-        this.props.fetchItems();
-    }
+  componentDidMount() {
+    this.props.fetchItems();
+  }
 
-    handleSubmit(event) {
-        this.setState({
-            loggedIn: true
-        });
-        event.preventDefault();
-    }
+  handleSubmit(event) {
+    this.setState({
+      loggedIn: true
+    });
+    event.preventDefault();
+  }
 
-    render() {
-        const HomePage = () => {
-            return <Home sellItems={this.props.sellItems.sellItems} />;
-        };
+  render() {
+    const HomePage = () => {
+      return <Home sellItems={this.props.sellItems.sellItems} />;
+    };
 
-        return (
-            <div>
-                <Header
-                    users={this.props.users}
-                    filterResults={this.props.filterResults}
-                    fetchItems={this.props.fetchItems}
-                />
-                <Switch>
-                    <Route exact path="/" component={HomePage} />
-                    <Redirect to="/" />
-                </Switch>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <Header
+          users={this.props.users}
+          filterResults={this.props.filterResults}
+          fetchItems={this.props.fetchItems}
+        />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/profile" component={Profile} />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default withRouter(
-    connect(
-        mapStateToProps,
-        mapDispatchToProps
-    )(Main)
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Main)
 );
--- src/components/ProfileComponent.jsx
+++ src/components/ProfileComponent.jsx
@@ -1,1 +1,43 @@
-//  TODO: Profiles
+import React, { Component } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavbarToggler,
+  Collapse,
+  NavItem,
+  Jumbotron,
+  Button,
+  ModalHeader,
+  Modal,
+  ModalBody,
+  Form,
+  FormGroup,
+  Input,
+  Label
+} from "reactstrap";
+
+class Profile extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Jumbotron>
+        <div className="container">
+          <div className="row row-header">
+            <div className="col-12 col-sm-6">
+              <h1>Hello Jerry BABA,</h1>
+              <p>Jerry BABA. </p>
+            </div>
+          </div>
+        </div>
+      </Jumbotron>
+    );
+  }
+}
+
+export default Profile;
